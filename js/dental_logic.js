4c8beb864510ae092efadb68edc6c26239eb73d4
pacharanero <marcusbaw@gmail.com>
2014-09-27 12:24:11 +0100

    dental logic first few lines

--- js/dental_logic.js
+++ js/dental_logic.js
@@ -1,15 +1,26 @@
-function cariesRAG (parameters) {
-	
-}
+$( document ).ready(function() {
 
-function periodontalRAG (parameters) {
+	function cariesRAG (parameters) {
+		var caries_red = ["#caries_clinical_lesions", "#caries_patient_symptoms"]
+		var caries_amber = ["#caries_patient_diet", "#caries_patient_plaque_control", "#caries_patient_sibling"]
+		
+		if (red things) {
+			return red
+		};
+		else if (amber things) {
+			return amber
+		}
+		else return green
+	}
 
-}
+	function periodontalRAG (parameters) {
+		
+	}
 
-function softTissueRAG (parameters) {
+	function softTissueRAG (parameters) {
+	}
 
-}
-
-function toothSurfaceLossRAG (parameters) {
+	function toothSurfaceLossRAG (parameters) {
+	}
 
 }
