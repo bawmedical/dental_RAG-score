33189abe04803140a4481257ad93d840bf1cd27a
Eva Barabas <b_seven_e@yahoo.co.uk>
2014-09-28 12:08:08 +0100

    checked results

--- js/dental_logic.js
+++ js/dental_logic.js
@@ -30,6 +30,7 @@ $(document).ready(function() {
         if($('#surface_loss_moderate').is(':checked')) {
             return 'amber';
         }
+
         var amber = [
             '#surface_loss_diet',
             '#surface_loss_brushing',
@@ -55,6 +56,7 @@ $(document).ready(function() {
         if($('#periodontal_moderate').is(':checked')) {
             return 'amber';
         }
+
         var amber = [
             '#periodontal_patient_plaque_control',
             '#periodontal_patient_smoking',
@@ -76,14 +78,19 @@ $(document).ready(function() {
         if($('#soft_tissue_patient_symptoms').is(':checked')) {
             return 'red';
         }
+
+        if ($('#soft_tissue_patient_site').is(':checked')) {
+            return 'red';
+        }
+
         if($('#soft_tissue_lesion').is(':checked')) {
             return 'amber';
         }
+
         var amber = [
-            '#soft_tissue_patient_site',
             '#soft_tissue_patient_tobacco_use',
             '#soft_tissue_patient_alchohol'
-        ]
+        ];
         for(i=0; i<amber.length; i++){
             var item = $(amber[i])
             if(item.is(':checked')) {
@@ -105,23 +112,112 @@ $(document).ready(function() {
         } else {
             label += ' label-success';
         }
-//        $(domElement).append('class', classname);
         console.log(domElement);
         $(domElement + '_result').text(message).attr('class', label);
-//        $(domElement).attr('class', classname)
-//        $(domElement + '_result').text(message).attr('class', label);
     }
 
-	$('#caries_submit').click(function(e){
+	$('#caries_clinical_lesions').click(function(e){
         setResult('#caries', cariesRAG());
     });
-	$('#surface_loss_submit').click(function(e){
+
+    $('#caries_patient_symptoms').click(function(e){
+        setResult('#caries', cariesRAG());
+    });
+
+    $('#caries_patient_diet').click(function(e){
+        setResult('#caries', cariesRAG());
+    });
+
+    $('#caries_patient_plaque_control').click(function(e){
+        setResult('#caries', cariesRAG());
+    });
+
+    $('#caries_patient_sibling').click(function(e){
+        setResult('#caries', cariesRAG());
+    });
+
+    $('#surface_loss_excessive').click(function(e){
         setResult('#surface_loss', toothSurfaceLossRAG());
     });
-	$('#periodontal_submit').click(function(e){
+
+    $('#surface_loss_patient_symptoms').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+    $('#surface_loss_moderate').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+    $('#surface_loss_commensurate').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+    $('#surface_loss_diet').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+    $('#surface_loss_para').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+    $('#surface_loss_reflux').click(function(e){
+        setResult('#surface_loss', toothSurfaceLossRAG());
+    });
+
+	$('#periodontal_severe').click(function(e){
         setResult('#periodontal', periodontalRAG());
     });
-	$('#cancer_submit').click(function(e){
+
+    $('#periodontal_patient_symptoms').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+    $('#periodontal_moderate').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+    $('#periodontal_healthy').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+    $('#periodontal_patient_plaque_control').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+    $('#periodontal_patient_smoking').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+    $('#periodontal_patient_poor_diabetes').click(function(e){
+        setResult('#periodontal', periodontalRAG());
+    });
+
+	$('#soft_tissue_referral').click(function(e){
         setResult('#cancer', softTissueRAG());
     });
+
+    $('#soft_tissue_patient_symptoms').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
+    $('#soft_tissue_patient_site').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
+    $('#soft_tissue_patient_tobacco_use').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
+    $('#soft_tissue_patient_alchohol').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
+    $('#soft_tissue_lesion').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
+    $('#soft_tissue_healthy').click(function(e){
+        setResult('#cancer', softTissueRAG());
+    });
+
 });
